01bad0473ba65b0d3eb44499de48ea5523ed31a4
victor-fdez <victor.j.fdez@gmail.com>
2013-03-10 11:50:19 -0400

    formatted xml base

--- FLLTutorial/src/main/java/com/honeybadgers/flltutorial/model/backend/XMLBase.java
+++ FLLTutorial/src/main/java/com/honeybadgers/flltutorial/model/backend/XMLBase.java
@@ -28,82 +28,86 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /*import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;*/
+ import org.dom4j.DocumentException;
+ import org.dom4j.Element;
+ import org.dom4j.io.SAXReader;*/
 /**
  *
  * @author Dan Doozan
  */
 public class XMLBase {
-    
+
     /**
-     * Load a Tutorial Base (ie. basic tutorial information, not the full tutorial) from an XML file.
-     * @param xmlFile   File to load Tutorial Base from.
-     * @return  The Tutorial Base loaded from the file.
+     * Load a Tutorial Base (ie. basic tutorial information, not the full
+     * tutorial) from an XML file.
+     *
+     * @param xmlFile File to load Tutorial Base from.
+     * @return The Tutorial Base loaded from the file.
      */
-    public static TutorialBase loadTutorialBase(File xmlFile){
+    public static TutorialBase loadTutorialBase(File xmlFile) {
         //todo: fill this in
         return null;
     }
-    
+
     /**
      * Loads a tutorial from the given XML file
-     * @param xmlFile  The XML file to read the tutorial from
-     * @return  Tutorial that was loaded from the file
+     *
+     * @param xmlFile The XML file to read the tutorial from
+     * @return Tutorial that was loaded from the file
      */
-    public static Tutorial loadTutorial(File xmlFile){
-        String name=null;
-        String mission=null;
+    public static Tutorial loadTutorial(File xmlFile) {
+        String name = null;
+        String mission = null;
         List<Stage> stages = new ArrayList<>();
 
-        try{
+        try {
             DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder(); //throws ParserConfigurationException
-            Document doc=docBuilder.parse(xmlFile); //throws SAXException, IOException
-            
+            Document doc = docBuilder.parse(xmlFile); //throws SAXException, IOException
+
             Element rootElement = (Element) doc.getElementsByTagName("tutorial").item(0);
             name = rootElement.getAttribute("name");
             //add author here
-            
+
             mission = rootElement.getElementsByTagName("mission").item(0).getTextContent();
-        
-            NodeList stageNodeList=doc.getElementsByTagName("stage");
-            for(int i=0;i<stageNodeList.getLength();i++){
-                Element stageElement = (Element)stageNodeList.item(i);
+
+            NodeList stageNodeList = doc.getElementsByTagName("stage");
+            for (int i = 0; i < stageNodeList.getLength(); i++) {
+                Element stageElement = (Element) stageNodeList.item(i);
                 Stage stage = loadStage(stageElement);
                 stages.add(stage);
                 printStage(stage);
             }
-        }catch(ParserConfigurationException | SAXException | IOException e){
+        } catch (ParserConfigurationException | SAXException | IOException e) {
             //handle exception here
         }
-        
+
         return new Tutorial(name, mission, stages);
     }
-    
-    
-    
+
     /**
      * Load a stage from the XML document
-     * @param stageElement  The root element of the stage tree
-     * @return  The loaded stage
+     *
+     * @param stageElement The root element of the stage tree
+     * @return The loaded stage
      */
     private static Stage loadStage(Element stageElement) {
         String name = stageElement.getAttribute("name");
         String videoPath = stageElement.getAttribute("video");
         long timeOnStage = Long.parseLong(stageElement.getAttribute("time")); //this is time that a student spent on this stage, in milliseconds
-        
-        Option rootOption = loadOption((Element)stageElement.getElementsByTagName("option").item(0));
-        
+
+        Option rootOption = loadOption((Element) stageElement.getElementsByTagName("option").item(0));
+
         return new Stage(name, rootOption, videoPath, timeOnStage);
     }
-    
+
     /**
-     * Recursive function to load an option and all sub-options from a given root option element
-     * @param optionElement  The option element to load from
-     * @return  The loaded Option
+     * Recursive function to load an option and all sub-options from a given
+     * root option element
+     *
+     * @param optionElement The option element to load from
+     * @return The loaded Option
      */
-    private static Option loadOption(Element optionElement){
+    private static Option loadOption(Element optionElement) {
         //default values
         String oid = "";
         boolean correct = true;
@@ -113,73 +117,82 @@ public class XMLBase {
         String reason = "";
         String imagePath = "";
         List<Option> subOptions = new ArrayList<>();
-        
+
         Option parent = null; //todo: set this to something
-        
+
         //load the attributes
-        if(optionElement.hasAttribute("oid"))
+        if (optionElement.hasAttribute("oid")) {
             oid = optionElement.getAttribute("oid");
-        if(optionElement.hasAttribute("correct"))
+        }
+        if (optionElement.hasAttribute("correct")) {
             correct = optionElement.getAttribute("correct").equalsIgnoreCase("true");
-        if(optionElement.hasAttribute("position"))
+        }
+        if (optionElement.hasAttribute("position")) {
             position = Integer.parseInt(optionElement.getAttribute("position"));
-        if(optionElement.hasAttribute("selected"))
+        }
+        if (optionElement.hasAttribute("selected")) {
             selected = Integer.parseInt(optionElement.getAttribute("selected"));
-        
+        }
+
         NodeList children = optionElement.getChildNodes();
-        for(int i=0; i<children.getLength(); i++){
-            if(children.item(i).getNodeType() == Node.ELEMENT_NODE){
-                Element childElement = (Element)children.item(i);
-                switch(childElement.getTagName()){
-                    case "desc": desc=childElement.getTextContent();
+        for (int i = 0; i < children.getLength(); i++) {
+            if (children.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                Element childElement = (Element) children.item(i);
+                switch (childElement.getTagName()) {
+                    case "desc":
+                        desc = childElement.getTextContent();
                         break;
-                    case "reason": reason=childElement.getTextContent();
+                    case "reason":
+                        reason = childElement.getTextContent();
                         break;
-                    case "img": imagePath=childElement.getAttribute("src");
+                    case "img":
+                        imagePath = childElement.getAttribute("src");
                         break;
-                    case "option": subOptions.add(loadOption(childElement));
+                    case "option":
+                        subOptions.add(loadOption(childElement));
                         break;
-                    
+
                 }
             }
         }
-        
+
         return new Option(desc, correct, subOptions, parent, oid, reason, position, selected, imagePath);
     }
-    
+
     /**
      * Save the given Tutorial to a new XML file.
-     * @param tutorial  The tutorial to save
+     *
+     * @param tutorial The tutorial to save
      */
-    public static void saveTutorial(Tutorial tutorial){
-        String name = tutorial.getName(); 
+    public static void saveTutorial(Tutorial tutorial) {
+        String name = tutorial.getName();
         String mission = tutorial.getMission();
         List<Stage> stages = tutorial.getStages();
-        
-        try{
+
+        try {
             DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder(); //throws ParserConfigurationException
-            Document doc=docBuilder.newDocument();
-            
+            Document doc = docBuilder.newDocument();
+
             Element rootElement = doc.createElement("tutorial");
             rootElement.setAttribute("name", name);
             doc.appendChild(rootElement);
-            
+
             //add mission element
             Element missionElement = doc.createElement("mission");
             missionElement.appendChild(doc.createTextNode(mission));
             rootElement.appendChild(missionElement);
-            
-            for(Stage stage : stages){
+
+            for (Stage stage : stages) {
                 rootElement.appendChild(constructStage(stage, doc));
             }
-            
+
             // write the content into xml file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("src/main/resources/sampleTutorial/"+name+".xml")); //***not sure if I can save here
+            StreamResult result = new StreamResult(new File("src/main/resources/sampleTutorial/" + name + ".xml")); //***not sure if I can save here
 
             // Output to console for testing
             //StreamResult result = new StreamResult(System.out);
@@ -187,31 +200,30 @@ public class XMLBase {
             transformer.transform(source, result); //throws TransformerException
 
             System.out.println("File saved!");
-            
-        }catch(ParserConfigurationException | TransformerException e){
+
+        } catch (ParserConfigurationException | TransformerException e) {
             //handle exception here
             e.printStackTrace();
         }
-        
+
     }
-    
-    
-    private static Element constructStage(Stage stage, Document doc){
+
+    private static Element constructStage(Stage stage, Document doc) {
         String name = stage.getName();
         String videoPath = stage.getVideoPath();
         long timeOnStage = stage.getTimeOnStage();
         Option rootOption = stage.getRootOption();
-        
+
         Element stageElement = doc.createElement("stage");
         stageElement.setAttribute("name", name);
         stageElement.setAttribute("video", videoPath);
         stageElement.setAttribute("time", String.valueOf(timeOnStage));
-        stageElement.appendChild(constructOption(rootOption,doc));
-        
+        stageElement.appendChild(constructOption(rootOption, doc));
+
         return stageElement;
     }
-    
-    private static Element constructOption(Option option, Document doc){
+
+    private static Element constructOption(Option option, Document doc) {
         String description = option.getDescription();
         String reason = option.getReason();
         String imagePath = option.getImagePath();
@@ -220,57 +232,56 @@ public class XMLBase {
         Option parentOption = option.getParent();
         int position = option.getPosition();
         int selected = option.getSelected();
-        
+
         Element optionElement = doc.createElement("option");
         optionElement.setAttribute("oid", id);
         optionElement.setAttribute("correct", String.valueOf(correct));
         optionElement.setAttribute("position", String.valueOf(position));
         optionElement.setAttribute("selected", String.valueOf(selected));
-        
+
         //add desc element
         Element descElement = doc.createElement("desc");
         descElement.appendChild(doc.createTextNode(description));
         optionElement.appendChild(descElement);
-        
+
         //add reason element
         Element reasonElement = doc.createElement("reason");
         reasonElement.appendChild(doc.createTextNode(reason));
         optionElement.appendChild(reasonElement);
-        
+
         //add img element
-        if(imagePath!=null){
+        if (imagePath != null) {
             Element imgElement = doc.createElement("img");
             imgElement.setAttribute("src", imagePath);
             optionElement.appendChild(imgElement);
         }
-        
+
         List<Option> subOptions = option.getOptions();
-        for(Option subOption : subOptions){
+        for (Option subOption : subOptions) {
             optionElement.appendChild(constructOption(subOption, doc));
         }
         return optionElement;
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Tutorial t = XMLBase.loadTutorial(new File("src/main/resources/sampleTutorial/tut1-Dan.xml"));
         XMLBase.saveTutorial(t);
     }
-    
-    private static void printStage(Stage stage){
+
+    private static void printStage(Stage stage) {
         System.out.println(stage.getName());
-        printOption(stage.getRootOption(),1);
+        printOption(stage.getRootOption(), 1);
     }
-    
-    private static void printOption(Option option, int level){
-        for(int i=0;i<level;i++){
+
+    private static void printOption(Option option, int level) {
+        for (int i = 0; i < level; i++) {
             System.out.print("\t");
         }
         System.out.println(option.getDescription());
-        if(option.getOptions()!=null){
-            for(Option op : option.getOptions()){
-                printOption(op, level+1);
+        if (option.getOptions() != null) {
+            for (Option op : option.getOptions()) {
+                printOption(op, level + 1);
             }
         }
     }
-    
 }
